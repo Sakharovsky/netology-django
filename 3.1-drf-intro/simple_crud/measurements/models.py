276b545fa9d2c1c89efd007f728af92b96c646d3
Sergey Sakharovsky <sergey.sakharovskiy@gmail.com>
2021-11-29 22:27:20 +0300

    3.1: feature: optional tasks

--- 3.1-drf-intro/simple_crud/measurements/models.py
+++ 3.1-drf-intro/simple_crud/measurements/models.py
@@ -1,28 +1,28 @@
 from django.db import models
 
+class TimestampFields(models.Model):
+    """Вынос меток времени"""
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
 
-class Project(models.Model):
+    class Meta:
+        abstract = True
+
+class Project(TimestampFields):
     """Объект на котором проводят измерения."""
 
     name = models.TextField()
     latitude = models.FloatField()
     longitude = models.FloatField()
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
 
 
-class Measurement(models.Model):
+class Measurement(TimestampFields):
     """Измерение температуры на объекте."""
 
     value = models.FloatField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
+    image = models.ImageField(null=True)
